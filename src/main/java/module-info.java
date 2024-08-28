507f655f0d1818d70fe3b600d4711f60a7f38fee
meowlena <milenabraga98@gmail.com>
2024-08-27 22:09:33 -0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.meowlena.collector {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.meowlena.collector to javafx.fxml;
+    exports com.meowlena.collector;
+}
